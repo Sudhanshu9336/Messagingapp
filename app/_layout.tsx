0fe11eb2226fdc7773cb8b4309dbce41ed9a9d20
Abhidroid87 <abhishekdroid87@gmail.com>
2025-09-04 19:19:35 +0530

    did some changes in socket

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,22 @@
+import 'react-native-get-random-values';
+import { decode, encode } from 'base-64';
+import * as Crypto from 'expo-crypto';
+
+// Polyfill crypto functions for React Native
+if (!global.atob) global.atob = decode;
+if (!global.btoa) global.btoa = encode;
+
+// Polyfill crypto.getRandomValues using expo-crypto
+if (!global.crypto) {
+  global.crypto = {
+    getRandomValues: (array: Uint8Array) => {
+      const randomBytes = Crypto.getRandomBytes(array.length);
+      array.set(randomBytes);
+      return array;
+    },
+  } as any;
+}
+
 import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
